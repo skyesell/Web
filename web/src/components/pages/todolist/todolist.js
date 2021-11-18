a44553d87795bc8fa1746e1c017361a13f584154
Skyesell <Skyesell@yandex.ru>
2021-11-18 13:41:43 +0300

    Done lab2

--- web/src/components/pages/todolist/todolist.js
+++ web/src/components/pages/todolist/todolist.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import Carddate from '../../UI/carddate/carddate'
+import React, { useState } from "react";
+import Carddate from '../../UI/carddate/carddate';
 import Cardfact from '../../UI/cardfact/cardfact';
 import Cardsuccess from '../../UI/cardsuccess/cardsuccess';
 import Cardtodo from '../../UI/cardtodo/cardtodo';
 import Sidebar from '../../UI/sidebar/sidebar';
 import Userbar from '../../UI/userbar/userbar';
+import Modal from "../../UI/modal/modal";
+import addtask from '../../img/icon/addtask.svg';
 import './style.css';
 
 
 
 
 export default function Todolist() {
+    const [show, setShow] = useState(false);
     return (
         <div>
 
@@ -24,9 +27,15 @@ export default function Todolist() {
                 <section class="right">
                     <Carddate />
                     <Cardfact />
+                    <button onClick={() => setShow(true)}><img class="iconadd" src={addtask} alt = "Error"/>Новая задача</button>
+                        <Modal title="Добавить новую задачу" onClose={() => setShow(false)} show={show}>
+                            <input type="text" required placeholder="Что нужно сделать?" />
+                         </Modal>
                 </section>
-                    
+            
             </main>
+            
+
         </div>
     )
 }
